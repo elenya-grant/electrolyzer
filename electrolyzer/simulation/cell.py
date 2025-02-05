212b0c03f6ae7594ebe2cd682212eb5371024f3d
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-02-05 10:47:32 -0700

    filling out base cell class and curve equation templates

--- electrolyzer/simulation/cell.py
+++ electrolyzer/simulation/cell.py
@@ -9,15 +9,18 @@ import pandas as pd
 import rainflow
 from attrs import field, define
 from scipy.signal import tf2ss, cont2discrete
-
+from scipy.constants import R, physical_constants
 from electrolyzer.tools.type_dec import NDArrayFloat, FromDictMixin, array_converter
 from electrolyzer.tools.validators import contains
+
+
+F, _, _ = physical_constants["Faraday constant"] 
 
 @define
 class Cell(FromDictMixin):
    
     # n: int = 2
-    
+
     # Constants
     #TODO: change to z_c
     z: int = 2 # number of electrons transferred in reaction
@@ -33,3 +36,82 @@ class Cell(FromDictMixin):
     lhv: float = 33.33  # lower heating value of H2 [kWh/kg]
     hhv: float = 39.41  # higher heating value of H2 [kWh/kg]
     gibbs: float = 237.24e3  # Gibbs Energy of global reaction (J/mol)
+
+    def calc_current_density(self,current):
+        j = current/self.cell_area
+        return j
+
+    def calc_cell_voltage(self,current,temperature):
+        E_cell = self.calc_open_circuit_voltage()
+        V_act_a, V_act_c = self.calc_activation_overpotential(current,temperature)
+        V_ohm = self.calc_ohmic_overpotential(current,temperature)
+        V_cell = E_cell + V_act_a + V_act_c + V_ohm
+        return V_cell
+
+    def calc_ohmic_overpotential(self,current,temperature):
+        R_tot = self.calc_total_resistance(current,temperature)
+        V_ohm = R_tot*current 
+        return V_ohm
+
+    def calc_activation_overpotential(self,current,temperature):
+        pass
+
+    def calc_open_circuit_voltage(self,temperature):
+        E_rev_0 = self.calc_reversible_voltage()
+        return E_rev_0
+
+    def calc_reversible_voltage(self):
+        return self.gibbs / (self.z * F)
+
+    def calc_total_resistance(self,current,temperature):
+        R_mem = self.calc_membrane_resistance()
+        R_c,R_a = self.calc_electrode_resistance()
+        R_elec = self.calc_electrolyte_resistance()
+
+        R_tot = R_mem + R_c + R_a + R_elec
+        return R_tot
+
+    def calc_membrane_resistance(self):
+        pass
+    
+    def calc_electrode_resistance(self):
+        pass
+
+    def calc_electrolyte_resistance(self):
+        pass
+    
+    def calc_thermoneutral_voltage(self):
+        pass
+
+    def calc_temperature(self):
+        pass
+
+    def calc_reverse_faradays(self,hydrogen_demand_per_cell):
+        # TODO: replace 2 with self.z
+        I_reqd_BOL_noFaradaicLoss=(hydrogen_demand_per_cell*1000*2*F)/(self.dt*self.M_H2)
+        n_f = self.calc_faradaic_efficiency(I_reqd_BOL_noFaradaicLoss)
+        I_reqd = (hydrogen_demand_per_cell*1000*2*F)/(n_f*self.dt*self.M_H2)
+        return I_reqd
+
+    def calc_faradaic_efficiency(self,current):
+        j = self.calc_current_density(current)
+        j *= 1000
+        eta_F = self.f_2 * (j**2) / (self.f_1 + j**2)
+        return eta_F
+
+    def calc_H2_mass_flow_rate(self,current):
+        # TODO: replace 2 with self.z
+        eta_F = self.calc_faradaic_efficiency(current)
+        h2_prod_mol = eta_F * current / (2 * F) #[mol/sec]
+        mfr = self.M_H2 * h2_prod_mol  # [g/sec]
+        mfr_H2 = self.dt*mfr / 1e3  # [kg/cell-dt]
+        return mfr_H2
+
+    def calc_O2_mass_flow_rate(self,current):
+        # TODO: replace 4 with ....
+        eta_F = self.calc_faradaic_efficiency(current)
+        o2_prod_mol = eta_F * current / (4 * F) #[mol/sec]
+        mfr = self.M_O2 * o2_prod_mol  # [g/sec]
+        mfr_O2 = self.dt*mfr / 1e3  # [kg/cell-dt]
+        return mfr_O2
+
--- electrolyzer/simulation/curve_equation_templates.py
+++ electrolyzer/simulation/curve_equation_templates.py
@@ -1,7 +1,16 @@
 
-def power_to_current_cubic_constant_temp(P_T,p1,p2,p3,p4,p5,p6): 
+def p_to_i_cubic_constant_temp(P_T,p1,p2,p3,p4,p5,p6): 
     #calculates i-v curve coefficients given the stack power and stack temp
     pwr,tempc=P_T
-    # i_stack=p1*(pwr**2) + p2*(tempc**2)+ (p3*pwr*tempc) +  (p4*pwr) + (p5*tempc) + (p6)
     i_stack=p1*(pwr**3) + p2*(pwr**2) +  (p3*pwr) + (p4*pwr**(1/2)) + p5
     return i_stack 
+
+def p_to_i_squared_variable_temp(P_T,a,b,c,d,e,f): 
+    """
+    Given a power input (kW), temperature (C), and set of coefficients, returns
+    current (A).  Coefficients can be determined using non-linear least squares
+    fit (see `Stack.create_polarization`).
+    """
+    pwr,tempc=P_T
+    i = (a * (pwr**2)) + (b * tempc**2) + (c * pwr * tempc) + (d * pwr) + (e * tempc) + f
+    return i
